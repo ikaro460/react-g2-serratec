bf1e1d0d1c1c3fe9c88c03c3b96199ad19c111c4
Icaro <ikaro_460@hotmail.com>
2023-11-06 20:12:40 -0300

    feat: context produtos

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,22 +1,44 @@
 import React, { createContext, useState } from "react";
+import { api } from "../services/api";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(localStorage.getItem("info"));
+  const clienteInfo = JSON.parse(localStorage.getItem("info"));
+  const produtosInfo = JSON.parse(localStorage.getItem("produtos"));
+
+  const [cliente, setCliente] = useState(clienteInfo ? clienteInfo : null);
+  const [produtos, setProdutos] = useState(produtosInfo ? produtosInfo : null);
 
   const logar = (loginData) => {
     console.log(loginData);
-    setUser(loginData);
+    setCliente(loginData);
   };
 
-  const deslogar = (loginData) => {
-    setUser(null);
+  const deslogar = () => {
+    setCliente(null);
+  };
+
+  const getProdutos = async () => {
+    try {
+      const { data } = await api.get("produto");
+      setProdutos(data);
+      localStorage.setItem("produtos", JSON.stringify(data));
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
     <AuthContext.Provider
-      value={{ user: user, estaLogado: !!user, logar, deslogar }}
+      value={{
+        cliente: cliente,
+        estaLogado: !!cliente,
+        logar,
+        deslogar,
+        produtos: produtos,
+        getProdutos,
+      }}
     >
       {children}
     </AuthContext.Provider>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,26 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import Radio1 from "../../components/Radio";
 import { AuthContext } from "../../context/AuthContext";
 
 export default function Home() {
-  const { user, deslogar } = useContext(AuthContext);
+  const { cliente, deslogar, getProdutos, produtos } = useContext(AuthContext);
+
+  useEffect(() => {
+    getProdutos();
+    console.log(produtos);
+  }, []);
 
   return (
     <>
       <h1>Home</h1>
-      <h2>Seja bem vindo(a), {user.nome}!</h2>
+      <h2>Seja bem vindo(a), {cliente.nome}!</h2>
       <button onClick={() => deslogar()}>Sair</button>
+      <ul>
+        {!!produtos &&
+          produtos.map((item, index) => {
+            return <li key={index}>{item.nome}</li>;
+          })}
+      </ul>
       <Radio1 />
     </>
   );
